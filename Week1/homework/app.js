d592be21ccbbf9649586c37d53abefeb5152753a
abdulrauf84 <abdul.rauf@live.se>
2019-08-23 01:34:52 +0200

    Objects added

--- Week1/homework/app.js
+++ Week1/homework/app.js
@@ -1,6 +1,5 @@
 'use strict';
 
-//2.1
 let bookTitles = [
   'Harry Potter',
   'The Great Gatsby',
@@ -12,82 +11,91 @@ let bookTitles = [
 ];
 //console.log(bookTitles);
 
-//2.3
-function booksList() {
-  let ul = document.createElement('ul');
-  let li = document.createElement('li');
-  bookTitles.map(book => {
-    let li = document.createElement('li');
-    li.appendChild(document.createTextNode(book));
-    return document.body.appendChild(ul.appendChild(li));
-  });
-}
-
-//2.4 - 2.7
-let books = [
-  {
+// function booksList() {
+//   let ul = document.createElement('ul');
+//   let li = document.createElement('li');
+//   bookTitles.map(book => {
+//     let li = document.createElement('li');
+//     li.appendChild(document.createTextNode(book));
+//     return document.body.appendChild(ul.appendChild(li));
+//   });
+// }
+let books = {
+  book1: {
     book_title: 'Harry Potter',
     author: 'J.K Rowling',
     language: 'English',
     cover_image: 'harrypotter.jpg',
   },
-  {
+  book2: {
     book_title: 'The Great Gatsby',
     author: 'F. Scott Fitzgerald',
     language: 'English',
     cover_image: 'thegreat.jpg',
   },
-  {
+  book3: {
     book_title: 'To Kill a Mockingbird',
     author: 'Harper Lee',
     language: 'English',
     cover_image: 'tokill.jpg',
   },
-  {
+  book4: {
     book_title: 'The Hobbit',
     author: 'J.R.R. Tolkien',
     language: 'English',
     cover_image: 'thehobbit.jpg',
   },
-  {
+  book5: {
     book_title: 'Fahrenheit 451',
     author: 'Ray Bradbury',
     language: 'English',
     cover_image: 'fahrenheit.jpg',
   },
-  {
+  book6: {
     book_title: 'The Catcher in the Rye',
     author: 'J.D. Salinger',
     language: 'English',
     cover_image: 'thecatcher.jpg',
   },
 
-  {
+  book7: {
     book_title: 'Pride and Prejudice',
     author: 'Jane Austen',
     language: 'English',
     cover_image: 'pap.jpg',
   },
-];
+};
+
+// for (let book in books) {
+//   console.log(books[book]['book_title']);
+//   console.log(books[book]['author']);
+//   console.log(books[book]['language']);
+// }
+
 function booksInfo() {
   let mainDiv = document.getElementById('main');
   let ul = document.createElement('ul');
 
-  books.map(book => {
+  for (let book in books) {
     let li = document.createElement('li');
     let img = document.createElement('IMG');
     let p = document.createElement('p');
-    img.src = './img/' + book.cover_image;
+    img.src = './img/' + books[book]['cover_image'];
     img.setAttribute('width', '250');
     img.setAttribute('height', '300');
-    img.setAttribute('alt', book.book_title);
-    li.appendChild(img);
-    p.innerHTML = `Title: ${book.book_title}<br/>Author: ${book.author}<br/>Language: ${
-      book.language
-    }`;
 
+    li.appendChild(img);
+    p.innerHTML =
+      'Title: ' +
+      books[book]['book_title'] +
+      '</br>' +
+      'Author: ' +
+      books[book]['author'] +
+      '</br>' +
+      'Language: ' +
+      books[book]['language'];
     li.appendChild(p);
     return mainDiv.append(ul.appendChild(li));
-  });
+  }
 }
 booksInfo();
